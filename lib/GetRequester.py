991f67c2815cf34e455b36ffd375d368a47bcae0
DidasMba <daniel.mbarushi@gmail.com>
2024-01-11 14:47:07 +0300

    addgetrequesterest

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,6 +1,4 @@
 import requests
-import pytest
-from lib.get_requester import GetRequester
 
 class GetRequester:
     def __init__(self, url):
@@ -13,14 +11,8 @@ class GetRequester:
     def load_json(self):
         response_body = self.get_response_body()
         try:
-            return response_body.json()  # Corrected line
+            return response_body.json()
         except ValueError:
             # Handle the case where the response is not a valid JSON
             print("Error: Unable to parse response as JSON.")
             return None
-
-def test_get_response():
-    '''get_response_body function returns response.'''
-    requester = GetRequester(URL)
-    actual_response = requester.get_response_body().decode('utf-8')  # Decode bytes to string
-    assert actual_response == JSON_STRING

44e11a8ba392f4926ea8d586dd5b641ab40fb88a
DidasMba <daniel.mbarushi@gmail.com>
2024-01-11 14:08:34 +0300

    reaquest

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,18 @@
 import requests
-import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            return response.json()
+        except ValueError:
+            # Handle the case where the response is not a valid JSON
+            print("Error: Unable to parse response as JSON.")
+            return None
